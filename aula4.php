a4cde289480a1e1aff9c3a2f60f95e012d4bdbfe
SophCSodre <sophia.c.sodre@hotmail.com>
2022-08-17 20:26:13 -0300

    aula4

---
+++ aula4.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = "20 de Agosto de 2020";    //string
+$salario = 850.00;   //float ou double
+$cargo = "Estagiario";   //string
+$idade = 18;  //integer ou int
+$resultado = true;  //boolean
+
+if ($resultado) {
+    echo "Verdadeiro";
+}
+
+?>
