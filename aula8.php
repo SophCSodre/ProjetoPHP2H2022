cc21c3032f5d824c0cab46fec4e66abad848bf26
SophCSodre <sophia.c.sodre@hotmail.com>
2022-09-05 20:22:26 -0300

    aula8

---
+++ aula8.php
@@ -0,0 +1,43 @@
+<?php
+
+$bruto = 1200;
+$inss = ;
+$por1 
+$ir;
+$grupo;
+$liquido;
+
+function $bruto 
+$por = 7,50;
+$result
+
+
+switch ($bruto) {
+
+    if 
+    case 1: $bruto = ($bruto , $num2);
+    $operacao = "soma";
+    break;
+
+    case 2: echo "subtraindo";
+    break;
+
+    case 3: echo "multiplicando";
+    break;
+
+    case 4: echo "dividindo";
+    break;
+    
+    default: echo "saindo";
+    break;
+}
+
+echo "o resultado da " .$operacao . " é " .$result;
+
+function somaValor ($num1, $num2){
+    return ($num1+$num2);
+}
+
+
+
+?>
