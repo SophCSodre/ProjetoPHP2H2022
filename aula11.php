111930cb25ee75c9f68a16db6b93188f1ec3f4ba
SophCSodre <sophia.c.sodre@hotmail.com>
2022-09-19 20:14:51 -0300

    Aula11

---
+++ aula11.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = 10;
+$num2 = 0;
+$result= 0;
+
+$result = $num1 + $num2;
+
+if ($result >= 20){
+    echo "O resultado é " .$result + 8; 
+}
+else if ($result <= 20){
+    echo "O resultado é " .$result - 5; 
+}
+
+?>
